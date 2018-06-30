fc38b02ae7888d2ce6a39ceb3f7cd21356241a78
Vasilis G <vasilisgoutz08@gmail.com>
2018-06-30 17:18:25 +0300

    Add files via upload

---
+++ Game of Life/Game_of_Life_Serial_CPU/Patterns.h
@@ -0,0 +1,37 @@
+#ifndef PATTERNS_H
+#define PATTERNS_H
+
+#define BHEPT1 "./Game of life pattern files/bhept1.txt"
+#define BHEPT2 "./Game of life pattern files/bhept2.txt"
+#define BHEPT3 "./Game of life pattern files/bhept3.txt"
+
+#define C3PUF  "./Game of life pattern files/c3puf.txt"
+#define C3PUF2  "./Game of life pattern files/c3puf2.txt"
+#define C3PUF7  "./Game of life pattern files/c3puf7.txt"
+#define C3PUF8  "./Game of life pattern files/c3puf8.txt"
+#define C3PUF18 "./Game of life pattern files/c3puf18.txt"
+
+#define DSPARK  "./Game of life pattern files/dspark.txt"
+
+#define P5SS  "./Game of life pattern files/p5ss.txt"
+
+#define P24GUN1 "./Game of life pattern files/p24gun1.txt"
+#define P24GUN2 "./Game of life pattern files/p24gun2.txt"
+#define P24GUN3 "./Game of life pattern files/p24gun3.txt"
+
+#define P57OSCILLATOR "./Game of life pattern files/p57oscillator.txt"
+#define P57OSCILLATOR2 "./Game of life pattern files/p57oscillator2.txt"
+
+#define RLE1 "./Game of life pattern files/rle1.txt"
+#define RLE2 "./Game of life pattern files/rle2.txt"
+#define RLE3 "./Game of life pattern files/rle3.txt"
+#define RLE4 "./Game of life pattern files/rle4.txt"
+#define RLE5 "./Game of life pattern files/rle5.txt"
+#define RLE18 "./Game of life pattern files/rle18.txt"
+#define RLE28 "./Game of life pattern files/rle28.txt"
+
+#define SPACESHIP1 "./Game of life pattern files/spaceship1.txt"
+
+#define SSREFL1 "./Game of life pattern files/ssrefl1.txt"
+
+#endif // PATTERNS_H
